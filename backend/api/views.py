398584a76ecaf3d56637da481e7409aec0b46f83
Srinath Silla <sillasrinath@gmail.com>
2024-03-07 18:46:06 -0500

    Echo GET DATA

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,18 @@
+import json
 from django.http import JsonResponse
 
+# request is from HttpRequest - Django which is different from requests which is python requests library
+
 def api_home(request):
-    return JsonResponse({"message":"Hi there!, this is your Django API Response"})
+    print(request.GET)  # url query parameters
+    body = request.body     # byte string of json data
+    data = {}
+    try:
+        data = json.loads(body) # string of json data -> python dict
+    except:
+        pass
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,14 +1,21 @@
 import requests
 
-endpoint = "http://127.0.0.1:8001/api"
+# always make sure to end url with trailing slash when sending params and request data over get request,
+endpoint = "http://127.0.0.1:8001/api/"
 
 #get_response = requests.get(endpoint)   # HTTP Request
 #get_response = requests.get(endpoint, json={"query":"Hello World!"})    # {'args': {}, 'data': '{"query": "Hello World!"}'..}
-get_response = requests.get(endpoint, data={"query":"Hello World!"})    #{'args': {}, 'data': '', 'files': {}, 'form': {'query': 'Hello World!'} ..}
+#get_response = requests.get(endpoint, data={"query":"Hello World!"})    #{'args': {}, 'data': '', 'files': {}, 'form': {'query': 'Hello World!'} ..}
 
+get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello World"})
 
-print(get_response.text)    # print raw text response
+#print(get_response.text)    # print raw text response
 
 print(get_response.json())  # prints json data
 
-print(get_response.status_code)
+#print(get_response.status_code)
+
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# Javascript object Notation ~ Python Dict
