dfebdadeb9879fe580157a8e23af245b4ab1cb22
Srinath Silla <sillasrinath@gmail.com>
2024-03-07 13:37:16 -0500

    Creating a python API client

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,15 @@
 import requests
 
+endpoint = "https://httpbin.org/status/200"
 endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint)
+#get_response = requests.get(endpoint)   # HTTP Request
+#get_response = requests.get(endpoint, json={"query":"Hello World!"})    # {'args': {}, 'data': '{"query": "Hello World!"}'..}
+get_response = requests.get(endpoint, data={"query":"Hello World!"})    #{'args': {}, 'data': '', 'files': {}, 'form': {'query': 'Hello World!'} ..}
 
-print(get_response.json())
+
+#print(get_response.text)    # print raw text response
+
+print(get_response.json())  # prints json data
+
+print(get_response.status_code)
