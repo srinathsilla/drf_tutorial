7305fbb29626c8ef36b69ca20eb3a8a1f444658d
Srinath Silla <sillasrinath@gmail.com>
2024-03-08 00:11:26 -0500

    Mixins and a Generic API View

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
@@ -48,6 +48,31 @@ class ProductListAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
 
 # function based view to implement CRUD operations, but not usually preferred because of how fast the code can get messy
 @api_view(['GET', 'POST'])
--- py_client/detail.py
+++ py_client/detail.py
@@ -1,6 +1,6 @@
 import requests
 
-endpoint = "http://127.0.0.1:8001/api/products/7/"
+endpoint = "http://127.0.0.1:8001/api/products/6/"
 
 get_response = requests.get(endpoint)
 
