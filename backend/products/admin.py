5b1d2b573329e935b5df100200c9690b113d0d04
Srinath Silla <sillasrinath@gmail.com>
2024-03-08 11:16:19 -0500

    User & Group Permissions with DjangoModelPermissions

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+admin.site.register(Product)
+
+# user credentials
+# cfe, password
+# staff, staffpassword
